15add23c48332e43ef55e84d73cc38690962e07e
David Iya <ekpodave@gmail.com>
2019-09-19 20:35:14 +0100

    Procfile and config

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,6 +22,8 @@ const sequelize = require('./sequelize');
 
 const app = express(feathers());
 
+require('dotenv').config();
+
 // Load app configuration
 app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
