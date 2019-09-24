1d4b50d2e304624ebacbe3435f47b89ff95a2091
David Iya <ekpodave@gmail.com>
2019-09-24 14:40:13 +0100

    Models

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -6,7 +6,24 @@ const DataTypes = Sequelize.DataTypes;
 module.exports = function (app) {
   const sequelizeClient = app.get('sequelizeClient');
   const users = sequelizeClient.define('users', {
-  
+    _id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_code: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    user_alias: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -16,14 +33,18 @@ module.exports = function (app) {
       type: DataTypes.STRING,
       allowNull: false
     },
-  
-  
-    googleId: { type: Sequelize.STRING },
-  
-    facebookId: { type: Sequelize.STRING },
-  
-    twitterId: { type: Sequelize.STRING },
-  
+    avatar: {
+      type: DataTypes.STRING,
+    },
+    googleId: {
+      type: Sequelize.STRING
+    },
+    facebookId: {
+      type: Sequelize.STRING
+    },
+    twitterId: {
+      type: Sequelize.STRING
+    },
   }, {
     hooks: {
       beforeCount(options) {
--- backend/src/services/users/users.class.js
+++ backend/src/services/users/users.class.js
@@ -1,5 +1,11 @@
 const { Service } = require('feathers-sequelize');
 
 exports.Users = class Users extends Service {
+  create(data, params) {
+    console.log("TCL: Users -> create -> data", data)
+    console.log("TCL: Users -> create -> params", params)
+
+    return super.create(data, params);
+  }
   
 };
